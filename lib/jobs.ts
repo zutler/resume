012d8cd0c2b00cc1dc2c7f17017456a750c9ccfd
Dmitriy Zutler <dmitriy.zutler@gmail.com>
2022-08-18 12:20:06 -0400

    deploy temporary slugs to staging

--- lib/jobs.ts
+++ lib/jobs.ts
@@ -1,3 +1,5 @@
+import { jobs } from '../data';
+
 const HOST = process.env.HOST;
 
 export const getSortedJobsData = async () => {
@@ -19,8 +21,16 @@ export const getSortedJobsData = async () => {
 };
 
 export async function getAllJobSlugs() {
-  const res = await fetch(`${HOST}/api/slugs`);
-  const data = await res.json();
+  // const res = await fetch(`${HOST}/api/slugs`);
+  // const data = await res.json();
+
+  type ResponseData =
+    | {
+        id: string;
+      }[]
+    | string;
+
+  const data = [...jobs.map((job) => ({ id: job?.id?.toString() }))];
 
   return data.map((job: { id: string }) => {
     return {

7b2737acb737827b79b3f558ce764c8b808d075d
Dmitriy Zutler <dmitriy.zutler@gmail.com>
2022-09-11 10:21:28 -0400

    address isolatedModules issue

--- cypress/e2e/pages/index.spec.cy.ts
+++ cypress/e2e/pages/index.spec.cy.ts
@@ -397,4 +397,6 @@ describe('index page', () => {
   });
 });
 
-export {}; //address the isolatedModules lint false positive
+//addressing the compilation issue:
+//Type error: 'commands.ts' cannot be compiled under '--isolatedModules' because it is considered a global script file. Add an import, export, or an empty 'export {}' statement to make it a module.
+export {};
